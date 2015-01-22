2f4fb2f1c00f04d4fe25cd77c17c385d1dd9d97c
KID <kid.wumeng@gmail.com>
2015-01-22 20:38:49 +0800

    add PHP.operator

--- PHP/operator.php
+++ PHP/operator.php
@@ -1,3 +1,47 @@
 <?php
 
-$x + $y;
+    $num = 5 + 2;
+    $num = 5 - 2;
+    $num = 5 * 2;
+    $num = 5 / 2;  // = 2.5
+    $num = 5 % 2;  // = 1
+
+
+    $num += 3;
+    $num -= 3;
+    $num *= 3;
+    $num /= 3;
+    $num %= 3;
+
+
+    ++$num;
+    --$num;
+    $num++;
+    $num--;
+
+
+    1 == '1';   // true
+    1 === '1';  // false
+
+    1 != '1';   // false
+    1 <> '1';   // false（效果同上）
+    1 !== '1';  // true
+
+    1 > '1';   // false
+    1 >= '1';  // true
+
+    1 < '1';   // false
+    1 <= '1';  // true
+
+
+    true && false;   // false
+    true and false;  // false（效果同上）
+
+    true || false;   // true
+    true or false;   // true（效果同上）
+
+    !true;           // false
+
+    true xor false;  // true
+
+?>
