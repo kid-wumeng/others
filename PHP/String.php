2493a3cafe8c00253f114fa22b81e2e1b6b7e6ef
KID <kid.wumeng@gmail.com>
2015-01-25 17:35:00 +0800

    update PHP.String

--- PHP/String.php
+++ PHP/String.php
@@ -33,16 +33,21 @@
 
 
     strlen('kid');  // 求字符串长度：3
+
     strpos('kid.wumeng@gmail.com', 'gmail');  // 定位第一次出现的位置：11
+
     strstr('kid.wumeng@gmail.com', 'gmail');  // 提取子串：'gmail.com'
+
     substr('kid.wumeng@gmail.com', 4);        // 提取子串：wumeng@gmail.com
     substr('kid.wumeng@gmail.com', 4, 6);     // 提取子串：wumeng
+
     str_replace('b', '*', 'abc abc abc', $count);  // 替换子串：'a*c abc a*c'（$count = 3，替换了3个，可选）
 
 
     // 字符串 -> 数组
     explode(',', 'Comic,Music,Sport');     // array('Comic','Music','Sport')
     explode(',', 'Comic,Music,Sport', 2);  // array('Comic','Music,Sport')
+
     // 数组 -> 字符串
     implode(',', array('Comic','Music','Sport'));  // 'Comic,Music,Sport'
     join(',', array('Comic','Music','Sport'));     // 'Comic,Music,Sport'
