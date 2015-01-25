b741b8dbea12013f241b8a4f8646aa10a840299c
KID <kid.wumeng@gmail.com>
2015-01-25 17:18:15 +0800

    update PHP.String

--- PHP/String.php
+++ PHP/String.php
@@ -5,6 +5,8 @@
     'I am ' . $name;
 
 
+
+
     /*
      * 单引号：绝对字面量
      * 双引号：会先计算变量、转义字符
@@ -17,6 +19,8 @@
 
     'hello \n world';  // hello \n world
     "hello \n world";  // hello world
+
+
 
 
     // 换行符 -> 换行标签（必须在双引号中）
