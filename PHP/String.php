97b52f03cf99bd2dca9eeb5ac5f6d9105ddc54b6
KID <kid.wumeng@gmail.com>
2015-01-25 17:29:33 +0800

    update PHP.String

--- PHP/String.php
+++ PHP/String.php
@@ -32,42 +32,19 @@
     urldecode('http%3A%2F%2Fwww.kid.me%2Findex.php%3Fname%3Dkid%26age%3D18');
 
 
-    // 求字符串长度
-    strlen('kid');  // 3
-
-
-    // 定位第一次出现的位置
-    strpos('kid.wumeng@gmail.com','gmail');  // 11
-
-
-    // 定位第一次出现的位置，提取从其开始的子串
-    strstr('kid.wumeng@gmail.com','gmail');  // 'gmail.com'
-
-
-    // 提取子串
-    substr('kid.wumeng@gmail.com', 4);  // 提取子串：wumeng@gmail.com
-    substr('kid.wumeng@gmail.com', 4, 6);  // 提取子串：wumeng
+    strlen('kid');  // 求字符串长度：3
+    strpos('kid.wumeng@gmail.com', 'gmail');  // 定位第一次出现的位置：11
+    strstr('kid.wumeng@gmail.com', 'gmail');  // 提取子串：'gmail.com'
+    substr('kid.wumeng@gmail.com', 4);        // 提取子串：wumeng@gmail.com
+    substr('kid.wumeng@gmail.com', 4, 6);     // 提取子串：wumeng
+    str_replace('b', '*', 'abc abc abc', $count);  // 替换子串：'a*c abc a*c'（$count = 3，替换了3个，可选）
 
 
     // 字符串 -> 数组
     explode(',', 'Comic,Music,Sport');     // array('Comic','Music','Sport')
     explode(',', 'Comic,Music,Sport', 2);  // array('Comic','Music,Sport')
-
-
     // 数组 -> 字符串
     implode(',', array('Comic','Music','Sport'));  // 'Comic,Music,Sport'
     join(',', array('Comic','Music','Sport'));     // 'Comic,Music,Sport'
-
-
-    // 替换子串（$count记录替换了多少，可选）
-    str_replace('b', '*', 'abc abc abc', $count);   // a*c abc a*c（$count = 3）
-
-
-    /*
-     * 不区分大小写的版本：
-     * strpos - stripos
-     * strstr - stristr
-     */
-
 
 ?>
