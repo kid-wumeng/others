65d6578ff3d3b8e1f6bf80121c4d2e303bed9067
KID <kid.wumeng@gmail.com>
2015-01-25 17:10:26 +0800

    update PHP.String

--- PHP/String.php
+++ PHP/String.php
@@ -25,4 +25,39 @@
     urlencode('http://www.kid.me/index.php?name=kid&age=18');                  // url编码
     urldecode('http%3A%2F%2Fwww.kid.me%2Findex.php%3Fname%3Dkid%26age%3D18');  // url解码
 
+
+    // 求字符串长度
+    strlen('kid');  // 3
+
+
+    // 定位第一次出现的位置
+    strpos('kid.wumeng@gmail.com','gmail');  // 11
+    strpos('kid.wumeng@gmail.com','163');  // false
+
+
+    // 定位第一次出现的位置，提取其之前的子串
+    strtok('kid.wumeng@gmail.com', '@');  // kid.wumeng
+
+
+    // 定位第一次出现的位置，提取从其开始的子串
+    strstr('kid.wumeng@gmail.com','gmail');  // 'gmail.com'
+
+
+    // 提取子串
+    substr('kid.wumeng@gmail.com', 4);  // 提取子串：wumeng@gmail.com
+
+
+    // 字符串 -> 数组
+    explode(',', 'Comic,Music,Sport');     // array('Comic','Music','Sport')
+    explode(',', 'Comic,Music,Sport', 2);  // array('Comic','Music,Sport')
+
+
+    // 数组 -> 字符串
+    implode(',', array('Comic','Music','Sport'));  // 'Comic,Music,Sport'
+    join(',', array('Comic','Music','Sport'));     // 'Comic,Music,Sport'
+
+
+    // 替换子串（$count记录替换了多少，可选）
+    str_replace('b', '*', 'abc abc abc', $count);   // a*c abc a*c（$count = 3）
+
 ?>
