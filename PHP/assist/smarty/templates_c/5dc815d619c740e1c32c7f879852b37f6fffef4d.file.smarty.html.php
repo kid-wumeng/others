075ad449757236e0adf15043ed1a3e3522d5b615
KID <kid.wumeng@gmail.com>
2015-01-29 18:54:26 +0800

    update PHP.Smarty

--- PHP/assist/smarty/templates_c/5dc815d619c740e1c32c7f879852b37f6fffef4d.file.smarty.html.php
+++ PHP/assist/smarty/templates_c/5dc815d619c740e1c32c7f879852b37f6fffef4d.file.smarty.html.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.21-dev, created on 2015-01-28 15:23:15
+<?php /* Smarty version Smarty-3.1.21-dev, created on 2015-01-29 10:54:18
          compiled from "assist\view\smarty.html" */ ?>
 <?php /*%%SmartyHeaderCode:3183054c8baf62fcc84-81477156%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '5dc815d619c740e1c32c7f879852b37f6fffef4d' => 
     array (
       0 => 'assist\\view\\smarty.html',
-      1 => 1422458590,
+      1 => 1422528856,
       2 => 'file',
     ),
   ),
@@ -37,22 +37,25 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 </head>
 <body>
 
+
     <h1>hello <?php echo $_smarty_tpl->tpl_vars['name']->value;?>
  ~ !</h1>
 
+
     <ul>
-    <?php  $_smarty_tpl->tpl_vars['val'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['val']->_loop = false;
+        <?php  $_smarty_tpl->tpl_vars['val'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['val']->_loop = false;
  $_from = $_smarty_tpl->tpl_vars['array_1']->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
 foreach ($_from as $_smarty_tpl->tpl_vars['val']->key => $_smarty_tpl->tpl_vars['val']->value) {
 $_smarty_tpl->tpl_vars['val']->_loop = true;
 ?>
         <li><?php echo $_smarty_tpl->tpl_vars['val']->value;?>
 </li>
-    <?php } ?>
+        <?php } ?>
     </ul>
 
+
     <ul>
-    <?php  $_smarty_tpl->tpl_vars['val'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['val']->_loop = false;
+        <?php  $_smarty_tpl->tpl_vars['val'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['val']->_loop = false;
  $_smarty_tpl->tpl_vars['key'] = new Smarty_Variable;
  $_from = $_smarty_tpl->tpl_vars['array_2']->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
 foreach ($_from as $_smarty_tpl->tpl_vars['val']->key => $_smarty_tpl->tpl_vars['val']->value) {
@@ -62,20 +65,22 @@ $_smarty_tpl->tpl_vars['val']->_loop = true;
         <li><?php echo $_smarty_tpl->tpl_vars['key']->value;?>
  ： <?php echo $_smarty_tpl->tpl_vars['val']->value;?>
 </li>
-    <?php } ?>
+        <?php } ?>
     </ul>
 
+
     <ul>
-    <?php  $_smarty_tpl->tpl_vars['man'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['man']->_loop = false;
+        <?php  $_smarty_tpl->tpl_vars['man'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['man']->_loop = false;
  $_from = $_smarty_tpl->tpl_vars['array_3']->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
 foreach ($_from as $_smarty_tpl->tpl_vars['man']->key => $_smarty_tpl->tpl_vars['man']->value) {
 $_smarty_tpl->tpl_vars['man']->_loop = true;
 ?>
         <li><?php echo $_smarty_tpl->tpl_vars['man']->value['name'];?>
-（<?php echo $_smarty_tpl->tpl_vars['man']->value['age'];?>
-岁）</li>
-    <?php } ?>
+ - <?php echo $_smarty_tpl->tpl_vars['man']->value['age'];?>
+岁</li>
+        <?php } ?>
     </ul>
+
 
 </body>
 </html>
