a631210e756a033f892cc9700268a1682bf9a285
KID <kid.wumeng@gmail.com>
2015-02-21 18:30:25 +0800

    update Go.base

--- Go/base.go
+++ Go/base.go
@@ -19,14 +19,14 @@ var (
     var_4   int16
     var_5   int32
     var_6   int64
-    var_7   rune  // 等同于int32
+    var_7   rune         // 等同于int32
     var_8   uint
     var_9   uint8
     var_10  uint16
     var_11  uint32
     var_12  uint64
-    var_13  uintptr  // 一个指针值的大小
-    var_14  byte     // 等同于uint8
+    var_13  uintptr      // 一个指针值的大小
+    var_14  byte         // 等同于uint8
     var_15  float32
     var_16  float64
     var_17  complex64   // 由float32类型的实部和虚部所组成的复数
