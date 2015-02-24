91e72b7db1eb100ee2b1e2ff2ecd55309cff177e
KID <kid.wumeng@gmail.com>
2015-02-24 15:50:06 +0800

    update Go.object-oriented

--- Go/object-oriented.go
+++ Go/object-oriented.go
@@ -1,52 +1,52 @@
 package main
 
 import (
-	. "fmt"
+    . "fmt"
 )
 
 
 
 type Coder interface {
-	Program()
-	Test(sth string)
+    Program()
+    Test(sth string)
 }
 
 type Animal struct {
-	age int
+    age int
 }
 
 // 相当于继承了Animal
 // 由于实现了Coder接口的全部方法，所以自动实现了Coder接口
 type Human struct {
-	Animal
-	name string
+    Animal
+    name string
 }
 
 func (human Human) Program() {
-	human.age = 0  // 值传递，原对象的age不会变
+    human.age = 0  // 值传递，原对象的age不会变
 }
 
 func (human *Human) Test() {
-	human.age = 0  // 引用传递，原对象的age会变
+    human.age = 0  // 引用传递，原对象的age会变
 }
 
 
 
 func main() {
 
-	human_1 := Human{ Animal{18}, "kid" }
+    human_1 := Human{ Animal{18}, "kid" }
 
-	human_2 := Human{
-		Animal: Animal{
-			age: 20,
-		},
-		name: "kid",
-	}
+    human_2 := Human{
+        Animal: Animal{
+            age: 20,
+        },
+        name: "kid",
+    }
 
-	// 属性、方法的访问权限与变量、函数一致
-	// 首字母大写：public，可被其它包访问
-	// 首字母小写：private，只能被本包访问
-	human_2.Program()
-	human_2.Test()
+    // 属性、方法的访问权限与变量、函数一致
+    // 首字母大写：public，可被其它包访问
+    // 首字母小写：private，只能被本包访问
+    human_2.Program()
+    human_2.Test()
 
 }
